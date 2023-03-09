9cc2d6689a718a92d839c3167eb2e7a7ca18160b
Nemanja <nemanja154@gmail.com>
2023-03-09 10:20:10 +0100

    work with dayjs

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,8 @@
 // ***********************************************
+const dayjs = require("dayjs");
+var isBetween = require('dayjs/plugin/isBetween')
+dayjs.extend(isBetween)
+
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
@@ -23,3 +27,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('isOlderThen', (numberOfMonth) => {
+    cy.request('https://novalite.rs/api/feed').then(response => {
+        const resBod = JSON.parse(response.body)
+
+        var today = dayjs().format('YYYY/MM/DD')
+        var numberOfMonthsAgo = dayjs().add(-numberOfMonth, 'month').format('YYYY/MM/DD')
+        
+        resBod.forEach(element => {
+        var elementDate = dayjs(element.created_time).format('YYYY/MM/DD')
+    
+         if(dayjs(elementDate).isBetween(numberOfMonthsAgo,today)){
+            expect(dayjs(elementDate).isBetween(numberOfMonthsAgo,today),
+             `Datum of element ${elementDate} is NOT OLDER then ${numberOfMonth} month`).to.eq(true)   
+            
+         }else{
+            expect(dayjs(elementDate).isBetween(numberOfMonthsAgo,today),
+             `Datum of element ${elementDate} IS OLDER then ${numberOfMonth}. month`).to.eq(false)
+         }
+                        
+        });
+    })
+})
